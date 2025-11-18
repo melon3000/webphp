46db8720409ec8796c48531921aa720408914645
melon3000 <milan.petrovski8@gmail.com>
2025-11-18 12:25:34 +0200

    uuenda css

--- content/ajaFunktsioonid.php
+++ content/ajaFunktsioonid.php
@@ -1,1 +1,56 @@
-<?php
+<h3>Ajafunktsioonid</h3>
+
+<div class="flex-container-ajaf">
+
+    <?php
+    // --- БЛОК 1 ---
+    echo "<div class='flex-row'>";
+    date_default_timezone_set('Europe/Tallinn');
+    echo "<a href='https://www.php.net/manual/en/timezones.php'>Timezone list</a><br>";
+    echo "time() - aeg sekundides - " . time() . "<br>";
+    echo "date() - " . date('d.m.Y H:i:s') . "<br>";
+    echo "</div>";
+
+    // --- БЛОК 2 ---
+    echo "<div class='flex-row'>";
+    echo "<pre>
+d - päev 01..31
+m - kuu 1..12
+Y - aasta - 2025++
+G - 24h formaat
+i - minutid 0..59
+s - sekundid 0..59
+</pre>";
+    echo "</div>";
+
+    // --- БЛОК 3 ---
+    echo "<div class='flex-row'>";
+    echo "<h4>Tehted Kuupäevaga</h4>";
+    echo "+1 min = " . date("d.m.Y H:i:s", time() + 60) . "<br>";
+    echo "+1 tund = " . date("d.m.Y H:i:s", time() + 3600) . "<br>";
+    echo "+1 päev = " . date("d.m.Y H:i:s", time() + 86400) . "<br>";
+    echo "</div>";
+
+    // --- БЛОК 4 ---
+    echo "<div class='flex-row'>";
+    echo "<h4>Kuupäeva genereerimine</h4>";
+    $synd = mktime(8, 43, 41, 5, 22, 2008);
+    echo "Minu sünnipäev on: " . date("d.m.Y H:i:s", $synd);
+    echo "</div>";
+
+    // --- БЛОК 5 ---
+    echo "<div class='flex-row'>";
+    echo "Massiivi abil näidata tänase kuu nimega<br>";
+    $kuud = [
+        1 => 'Jaanuar', 'Veebruar', 'Märts', 'April', 'Mai',
+        'Juuni', 'Juuli', 'August', 'September', 'Oktober',
+        'November', 'December'
+    ];
+    $aasta = date("Y");
+    $paev = date("d");
+    $kuu = $kuud[date("n")];
+    echo "Täna on $paev $kuu $aasta";
+    echo "</div>";
+    ?>
+
+</div>
--- nav.php
+++ nav.php
@@ -3,6 +3,7 @@
         <li><strong><a href="?link=Avaleht.php">Avaleht</a></strong></li>
         <li><strong><a href="?link=gitKasutamine.php">Git käsud</a></strong></li>
         <li><strong><a href="?link=Kusitlus.php">Küsitlus</a></strong></li>
+        <li><strong><a href="?link=ajaFunktsioonid.php">Aja funktsioonid</a></strong></li>
         <li><strong><a href="https://milanpetrovski24.thkit.ee" target="_blank">Vana index ⇲</a></strong></li>
     </ul>
 </nav>
