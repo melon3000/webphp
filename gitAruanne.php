ecb6765bb5430b14f61c8ba527799a24a0fb4e56
melon3000 <milan.petrovski8@gmail.com>
2025-11-18 09:35:06 +0200

    Fail gitAruanne on lisatud.

--- gitAruanne.php
+++ gitAruanne.php
@@ -28,5 +28,23 @@ echo "</li>";
         git push
     </pre>
 </li>
+
 <?php
 echo "<li>GITHUB projektiga sidumine>";
+echo "<pre>";
+echo "
+git remote add origin git@github.com:melon3000/webphp.git
+git branch -M main
+git push -u origin main
+";
+echo "</pre>";
+echo "</li>";
+
+echo "<li>Projektide kloneerimine desktopi.<br>
+* kontrolli, et id_rsa võti on olemas .ssh kaustas<br>
+* GIT CMD on lahti ja <br>";
+echo"<pre>";
+echo "git clone git@github.com:melon3000/webphp.git";
+echo "</pre>";
+echo "</li>";
+echo "</ol>";
