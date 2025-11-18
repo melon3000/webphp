6471f54488f9b8c79288eb30fd67529b5bb822e7
melon3000 <milan.petrovski8@gmail.com>
2025-11-18 09:18:54 +0200

    index fail on loodud

---
+++ gitAruanne.php
@@ -0,0 +1,32 @@
+<?php
+echo "<h2>GIT käsud</h2>";
+echo "<ol>";
+echo "<li>Repo loomine";
+echo "<pre>git init</pre>";
+echo "</li>";
+?>
+<li>Konfigureerimine
+    <pre>
+        git config --global user.name "melon3000"
+        git config --global user.email "milan.petrovski8@gmail.com"
+        git config --global --list
+    </pre>
+</li>
+<li>
+    Ssh võti loomine
+    <pre>
+        ssh-keygen -o -t rsa -C "milan.petrovski8@gmail.com"
+    </pre>
+    id_rsa.pub võti kopeeritakse githubi nägu deploy key
+</li>
+<li>
+    Jälgimise lisamine ja commit'i tegemine
+    <pre>
+        git status
+        git add .
+        git commit -a -m "{text}"
+        
+    </pre>
+</li>
+<?php
+echo "<li>GITHUB projektiga sidumine>";
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    echo "<h1>";
+    echo "PHP!";
+    echo "</h1>";
+    $text="Tarid";
+    echo $text;
+?>
