95789bceddb5e389600e6bd438d6de4f5d33845e
melon3000 <milan.petrovski8@gmail.com>
2025-11-25 09:00:44 +0200

    matem funkts lisatud

--- nav.php
+++ nav.php
@@ -4,6 +4,7 @@
         <li><strong><a href="?link=gitKasutamine.php">Git käsud</a></strong></li>
         <li><strong><a href="?link=Kusitlus.php">Küsitlus</a></strong></li>
         <li><strong><a href="?link=ajaFunktsioonid.php">Aja funktsioonid</a></strong></li>
+        <li><strong><a href="?link=matemfunkts.php">Matem funktsioonid</a></strong></li>
         <li><strong><a href="https://milanpetrovski24.thkit.ee" target="_blank">Vana index ⇲</a></strong></li>
     </ul>
 </nav>
